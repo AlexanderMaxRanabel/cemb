d7c6a088716b18a28cc7ab49092a4adb4340f42a
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-08-24 12:42:27 +0300

    Removed type_system.rs due to memory management issues

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-mod type_system;
-
 use std::env;
 
 use std::fs::File;
@@ -31,18 +29,52 @@ fn main() -> Result<(), std::io::Error> {
                                     if let Some(&var_type) = tokens.get(3) {
                                         match var_type {
                                             "String" => {
-                                                type_system::string_type(tokens, stack.clone(), var_name, var_type);
+                                                if let Some(&string_first_part) = tokens.get(5) {
+                                                    if let Some(&string_last_part) = tokens.last() {
+                                                        if string_first_part.starts_with("'") && string_last_part.ends_with("'") {
+                                                            let value: String = tokens[5..].join(" ");
+                                                            let metadata: String = format!("{}, {}, {}", var_name, var_type, value);
+                                                            stack.push(metadata);
+                                                        } else {
+                                                            println!("Usage: let str :: String = 'Hello world'");
+                                                            std::process::exit(0);
+                                                        }
+                                                    }
+                                                }
                                             },
                                             "Int" => {
-                                                type_system::integer_type(tokens, stack.clone(), var_name, var_type);
+                                                if let Some(&value) = tokens.get(5) {
+                                                    if let Ok(_number) = value.parse::<i32>() {
+                                                        let metadata: String = format!("{}, {}, {}", var_name, var_type, value);
+                                                        stack.push(metadata);
+                                                        println!("{:?}", stack);
+                                                    } else {
+                                                        println!("{} {}", "Not a piece of valid integer: ".red(), value);
+                                                    }
+                                                }
                                             },
 
                                             "Float" => {
-                                                type_system::float_type(tokens, stack.clone() , var_name, var_type);
+                                                if let Some(&value) = tokens.get(5) {
+                                                    if let Ok(_number) = value.parse::<f64>() {
+                                                        let metadata: String = format!("{}, {}, {}", var_name, var_type, value);
+                                                        stack.push(metadata);
+                                                        println!("{:?}", stack)
+                                                    } else {
+                                                        println!("{} {}", "Not a piece of float: ".red(), value);
+                                                    }
+                                                }
                                             },
 
                                             "Char" => {
-                                                type_system::char_type(tokens, stack.clone(), var_name, var_type);
+                                                if let Some(&value) = tokens.get(5) {
+                                                    if value.len() == 1 {
+                                                        let metadata: String = format!("{}, {}, {}", var_name, var_type, value);
+                                                        stack.push(metadata);
+                                                    } else {
+                                                        println!("{}: {}", "Not a valid piece of char due to length".red(), value);
+                                                    }
+                                                }
                                             },
                                              _ => {
                                                 println!("{} {}", "Error: Unknown Type: ".red(), var_type);
