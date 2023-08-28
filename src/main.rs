c06690f4557095715db242c90abfd7b880225509
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-08-28 10:30:31 +0300

    Working on if keyword

--- src/main.rs
+++ src/main.rs
@@ -215,21 +215,9 @@ fn main() -> Result<(), std::io::Error> {
                                                 let second_comparable_type = &metadata_array[1];
 
                                                 if first_comparable_type == second_comparable_type {
-                                                    match operator {
-                                                        //if x <operator> y <code>
-                                                        "==" => {
-                                                            if first_comparable_value == second_comparable_value {
-                                                                if let Some(&code_executable) = tokens.get(5) {
+                                                    if first_comparable_value == second_comparable_value {
+                                                        if let Some(&code_executable) = tokens.get(5) {
 
-                                                                }
-                                                            }
-                                                        },
-                                                        "!=" => {},
-                                                        "<" =>  {},
-                                                        ">" => {},
-                                                        _ => {
-                                                            println!("{} Not a valid operator: {}", "Error:".red(), operator);
-                                                            std::process::exit(1);
                                                         }
                                                     }
                                                 } else {
