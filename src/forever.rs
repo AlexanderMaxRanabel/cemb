d56c7eb358f42a33d125036c086f07d164ebfb12
choroalp <choroalp@skiff.com>
2023-12-03 15:05:49 +0300

    Added variable declaration support to other functions

--- src/forever.rs
+++ src/forever.rs
@@ -7,6 +7,105 @@ pub fn forever(tokens: Vec<&str>, mut stack: Vec<String>) -> Vec<String> {
         let forever_loop_executable_code: Vec<&str> = tokens[1..].to_vec();
         loop {
             match main_keyword {
+                "let" => {
+                    //let0 x1 ::2 int3 =4 45
+                    if let Some(&var_name) = tokens.get(1) {
+                        if let Some(&type_indicator) = tokens.get(2) {
+                            if type_indicator == "::" {
+                                if let Some(&var_type) = tokens.get(3) {
+                                    match var_type {
+                                        "String" => {
+                                            if let Some(&string_first_part) = tokens.get(5) {
+                                                if let Some(&string_last_part) = tokens.last() {
+                                                    if string_first_part.starts_with("'")
+                                                        && string_last_part.ends_with("'")
+                                                    {
+                                                        let value: String = tokens[5..].join(" ");
+                                                        let metadata: String = format!(
+                                                            "{} {} {}",
+                                                            var_name, var_type, value
+                                                        );
+                                                        stack.push(metadata);
+                                                    } else {
+                                                        println!("Usage: let str :: String = 'Hello world'");
+                                                        std::process::exit(0);
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                        "Int" => {
+                                            if let Some(&value) = tokens.get(5) {
+                                                if let Ok(_number) = value.parse::<i64>() {
+                                                    let metadata: String = format!(
+                                                        "{} {} {}",
+                                                        var_name, var_type, value
+                                                    );
+                                                    stack.push(metadata);
+                                                } else {
+                                                    println!(
+                                                        "{} {}",
+                                                        "Not a piece of valid integer: ".red(),
+                                                        value
+                                                    );
+                                                }
+                                            }
+                                        }
+
+                                        "Float" => {
+                                            if let Some(&value) = tokens.get(5) {
+                                                if let Ok(_number) = value.parse::<f64>() {
+                                                    let metadata: String = format!(
+                                                        "{} {} {}",
+                                                        var_name, var_type, value
+                                                    );
+                                                    stack.push(metadata);
+                                                } else {
+                                                    println!(
+                                                        "{} {}",
+                                                        "Not a piece of float: ".red(),
+                                                        value
+                                                    );
+                                                }
+                                            }
+                                        }
+
+                                        "Char" => {
+                                            if let Some(&value) = tokens.get(5) {
+                                                if value.len() == 1 {
+                                                    let metadata: String = format!(
+                                                        "{} {} {}",
+                                                        var_name, var_type, value
+                                                    );
+                                                    stack.push(metadata);
+                                                } else {
+                                                    println!(
+                                                        "{}: {}",
+                                                        "Not a valid piece of char due to length"
+                                                            .red(),
+                                                        value
+                                                    );
+                                                }
+                                            }
+                                        }
+
+                                        _ => {
+                                            println!(
+                                                "{} {}",
+                                                "Error: Unknown Type: ".red(),
+                                                var_type
+                                            );
+                                            std::process::exit(0)
+                                        }
+                                    }
+                                }
+                            } else {
+                                println!("Usage: let x :: TYPE = VALUE");
+                            }
+                        }
+                    }
+                }
+
                 "printline" => {
                     printline::printline(forever_loop_executable_code.clone(), stack.clone());
                 }
--- src/while_loop.rs
+++ src/while_loop.rs
@@ -4,6 +4,94 @@ use colored::*;
 
 pub fn runner(tokens: Vec<&str>, mut stack: Vec<String>) -> Vec<String> {
     match tokens[0] {
+        "let" => {
+            //let0 x1 ::2 int3 =4 45
+            if let Some(&var_name) = tokens.get(1) {
+                if let Some(&type_indicator) = tokens.get(2) {
+                    if type_indicator == "::" {
+                        if let Some(&var_type) = tokens.get(3) {
+                            match var_type {
+                                "String" => {
+                                    if let Some(&string_first_part) = tokens.get(5) {
+                                        if let Some(&string_last_part) = tokens.last() {
+                                            if string_first_part.starts_with("'")
+                                                && string_last_part.ends_with("'")
+                                            {
+                                                let value: String = tokens[5..].join(" ");
+                                                let metadata: String =
+                                                    format!("{} {} {}", var_name, var_type, value);
+                                                stack.push(metadata);
+                                            } else {
+                                                println!(
+                                                    "Usage: let str :: String = 'Hello world'"
+                                                );
+                                                std::process::exit(0);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                "Int" => {
+                                    if let Some(&value) = tokens.get(5) {
+                                        if let Ok(_number) = value.parse::<i64>() {
+                                            let metadata: String =
+                                                format!("{} {} {}", var_name, var_type, value);
+                                            stack.push(metadata);
+                                        } else {
+                                            println!(
+                                                "{} {}",
+                                                "Not a piece of valid integer: ".red(),
+                                                value
+                                            );
+                                        }
+                                    }
+                                }
+
+                                "Float" => {
+                                    if let Some(&value) = tokens.get(5) {
+                                        if let Ok(_number) = value.parse::<f64>() {
+                                            let metadata: String =
+                                                format!("{} {} {}", var_name, var_type, value);
+                                            stack.push(metadata);
+                                        } else {
+                                            println!(
+                                                "{} {}",
+                                                "Not a piece of float: ".red(),
+                                                value
+                                            );
+                                        }
+                                    }
+                                }
+
+                                "Char" => {
+                                    if let Some(&value) = tokens.get(5) {
+                                        if value.len() == 1 {
+                                            let metadata: String =
+                                                format!("{} {} {}", var_name, var_type, value);
+                                            stack.push(metadata);
+                                        } else {
+                                            println!(
+                                                "{}: {}",
+                                                "Not a valid piece of char due to length".red(),
+                                                value
+                                            );
+                                        }
+                                    }
+                                }
+
+                                _ => {
+                                    println!("{} {}", "Error: Unknown Type: ".red(), var_type);
+                                    std::process::exit(0)
+                                }
+                            }
+                        }
+                    } else {
+                        println!("Usage: let x :: TYPE = VALUE");
+                    }
+                }
+            }
+        }
+
         "printline" => {
             printline::printline(tokens.clone(), stack.clone());
         }
@@ -42,14 +130,20 @@ pub fn while_loop(tokens: Vec<&str>, mut stack: Vec<String>) -> Vec<String> {
                                     match mode {
                                         "+" => {
                                             while parsed_value < parsed_bound {
-                                                stack = runner(while_loop_executable_code.clone(), stack.clone());
+                                                stack = runner(
+                                                    while_loop_executable_code.clone(),
+                                                    stack.clone(),
+                                                );
                                                 parsed_value += parsed_rate;
                                             }
                                         }
 
                                         "-" => {
                                             while parsed_value > parsed_bound {
-                                                stack = runner(while_loop_executable_code.clone(), stack.clone());
+                                                stack = runner(
+                                                    while_loop_executable_code.clone(),
+                                                    stack.clone(),
+                                                );
                                                 parsed_value -= parsed_rate;
                                             }
                                         }
